fc42f346d57cbf83728576c1ccc40fdf7a5db536
andrii-kovsh <kobannn@gmail.com>
2023-08-21 23:12:23 +0300

    Updated the code considering given comments

--- src/test/java/cc/robotdreams/PersonTest.java
+++ src/test/java/cc/robotdreams/PersonTest.java
@@ -16,23 +16,29 @@ public class PersonTest {
     }
 
     public void testIsRetiredForMan() {
-        Man man = new Man("Василь", "Чірт", 30);
-        Assert.assertTrue(man.isRetired(), "Іван Петров повинен бути на пенсії");
+        Man man1 = new Man("Василь", "Чірт", 65);
+        Man man2 = new Man("Петро", "Коваль", 66);
+
+        Assert.assertTrue(man1.isRetired(), "Василь Чірт на пенсії");
+        Assert.assertTrue(man2.isRetired(), "Петро Коваль на пенсії");
     }
 
     public void testIsNotRetiredForMan() {
-        Man man = new Man("Ігор", "Сидоренко", 50);
-        Assert.assertFalse(man.isRetired(), "Ігор Сидоренко ще не на пенсії");
+        Man man = new Man("Володимир", "Сидоренко", 64);
+        Assert.assertFalse(man.isRetired(), "Володимир Сидоренко ще не на пенсії");
     }
 
     public void testIsRetiredForWoman() {
-        Woman woman = new Woman("Ольга", "Коваленко", 65);
-        Assert.assertTrue(woman.isRetired(), "Ольга Коваленко повинна бути на пенсії");
+        Woman woman1 = new Woman("Іра", "Дика", 60);
+        Woman woman2 = new Woman("Ольга", "Лисенко", 61);
+
+        Assert.assertTrue(woman1.isRetired(), "Іра Дика на пенсії");
+        Assert.assertTrue(woman2.isRetired(), "Ольга Лисенко повинна бути на пенсії");
     }
 
     public void testIsNotRetiredForWoman() {
-        Woman woman = new Woman("Марія", "Іваненко", 45);
-        Assert.assertFalse(woman.isRetired(), "Марія Іваненко ще не на пенсії");
+        Woman woman = new Woman("Тетяна", "Мельник", 59);
+        Assert.assertFalse(woman.isRetired(), "Тетяна Мельник ще не на пенсії");
     }
 
     public void testRegisterPartnershipForManAndWoman() {
